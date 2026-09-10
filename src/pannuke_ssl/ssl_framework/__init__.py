bb24c3176388a1e83987b89abe1d1cbd78a520c3
xw18958 <48813633+xw18958@users.noreply.github.com>
2026-09-10 16:27:33 +1000

    Expose standard SSL report generator

--- src/pannuke_ssl/ssl_framework/__init__.py
+++ src/pannuke_ssl/ssl_framework/__init__.py
@@ -4,4 +4,5 @@ from .data_validation import Validator,build_ssl_loader,module_sha
 from .trainer import train_ssl
 from .tuning import run_tuning
 from .downstream import run_downstream
-__all__=["apply_lr","load_standard_config","load_tuning_spec","EarlyStopper","Validator","build_ssl_loader","module_sha","train_ssl","run_tuning","run_downstream"]
+from .reporting import write_final_report
+__all__=["apply_lr","load_standard_config","load_tuning_spec","EarlyStopper","Validator","build_ssl_loader","module_sha","train_ssl","run_tuning","run_downstream","write_final_report"]
