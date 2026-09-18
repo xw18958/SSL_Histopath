807a15f09280c451921c9e46983c0af91a840f1b
xw18958 <48813633+xw18958@users.noreply.github.com>
2026-09-18 23:23:39 +1000

    Export tuned hyperparameter helper

--- src/pannuke_ssl/ssl_framework/__init__.py
+++ src/pannuke_ssl/ssl_framework/__init__.py
@@ -1,8 +1,8 @@
-from .config import apply_lr,load_standard_config,load_tuning_spec
+from .config import apply_lr,apply_tuned_hyperparameters,load_standard_config,load_tuning_spec
 from .early import EarlyStopper
 from .data_validation import Validator,build_ssl_loader,module_sha
 from .trainer import train_ssl
 from .tuning import run_tuning
 from .downstream import run_downstream
 from .reporting import write_final_report
-__all__=["apply_lr","load_standard_config","load_tuning_spec","EarlyStopper","Validator","build_ssl_loader","module_sha","train_ssl","run_tuning","run_downstream","write_final_report"]
+__all__=["apply_lr","apply_tuned_hyperparameters","load_standard_config","load_tuning_spec","EarlyStopper","Validator","build_ssl_loader","module_sha","train_ssl","run_tuning","run_downstream","write_final_report"]
